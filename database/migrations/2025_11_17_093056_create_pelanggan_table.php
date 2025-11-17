3343b041573dbeaff8a89a617aab99193c8c0c52
Muhamad Irfan <muhi66435@gmail.com>
2025-11-17 16:39:04 +0700

    'sembarang'

---
+++ database/migrations/2025_11_17_093056_create_pelanggan_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pelanggan', function (Blueprint $table) {
+            $table->id('id_pelanggan');
+            $table->foreignId('id_sales')->constrained('sales');
+            $table->string('nama');
+            $table->string('nik'); // fleksibel
+            $table->text('alamat');
+            $table->string('nomor_hp');
+            $table->string('ip_address');
+            $table->enum('status_pelanggan', ['baru', 'aktif', 'berhenti']);
+            $table->date('tanggal_registrasi');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pelanggan');
+    }
+};
---
+++ database/migrations/2025_11_17_093153_create_paket_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('paket', function (Blueprint $table) {
+            $table->id('id_pelanggan');
+            $table->string('nama_paket');
+            $table->string('kecepatan');
+            $table->integer('harga_dasar');
+            $table->decimal('ppn_nominal', 10, 2);
+            $table->integer('harga_total');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('paket');
+    }
+};

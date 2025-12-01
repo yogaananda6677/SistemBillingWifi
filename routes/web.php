deccd44fee8e3b368a94810ce7a5fe8b510cc580
yogaananda6677 <yogaanandaxx1212@gmail.com>
2025-12-01 13:37:25 +0700

    upload

--- routes/web.php
+++ routes/web.php
@@ -1,25 +1,23 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminTagihanController;
+use App\Http\Controllers\AreaController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\PaketController;
 use App\Http\Controllers\PelangganController;
+use App\Http\Controllers\PembayaranController;
+use App\Http\Controllers\PengajuanController;
 use App\Http\Controllers\PpnController;
 use App\Http\Controllers\SalesController;
-use App\Http\Controllers\PengajuanController;
-use App\Http\Controllers\AreaController;
-use App\Http\Controllers\PaketController;
-use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\TagihanController;
-use App\Http\Controllers\AdminTagihanController;
-use App\Http\Controllers\PembayaranController;
-
-
+use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 // ===== ADMIN ROUTES =====
-Route::middleware(['auth','admin'])->group(function () {
+Route::middleware(['auth', 'admin'])->group(function () {
     // Dashboard admin
     // Route::get('/dashboard/admin', function(){
     //     return view('admin.dashboard');
@@ -29,7 +27,7 @@ Route::middleware(['auth','admin'])->group(function () {
 
     // Resource pelanggan untuk admin
     Route::resource('pelanggan', PelangganController::class);
-// Halaman status pelanggan (menu terpisah)
+    // Halaman status pelanggan (menu terpisah)
     Route::get('/pelanggan-status', [PelangganController::class, 'status'])
         ->name('pelanggan.status');
 
@@ -44,7 +42,7 @@ Route::middleware(['auth','admin'])->group(function () {
         ->name('pelanggan.berhenti');
     Route::get('/get-sales-by-area/{id_area}', [SalesController::class, 'getSalesByArea']);
     Route::delete('/tagihan/hapus-pelanggan', [TagihanController::class, 'hapusTagihanPelanggan'])
-    ->name('tagihan.hapus-pelanggan');
+        ->name('tagihan.hapus-pelanggan');
 
     // halaman list tagihan belum lunas (pembayaran oleh admin)
     Route::get('/admin/tagihan', [AdminTagihanController::class, 'index'])
@@ -58,46 +56,32 @@ Route::middleware(['auth','admin'])->group(function () {
     Route::post('/admin/tagihan/bayar-banyak', [AdminTagihanController::class, 'bayarBanyak'])
         ->name('admin.tagihan.bayar-banyak');
 
+    Route::get('/pembayaran/riwayat', [PembayaranController::class, 'riwayat'])
+        ->name('pembayaran.riwayat');
 
-Route::get('/pembayaran/riwayat', [PembayaranController::class, 'riwayat'])
-    ->name('pembayaran.riwayat');
-
-
-Route::delete('/pembayaran/item/{id}', [PembayaranController::class, 'hapusItem'])
-    ->name('pembayaran.item.destroy');
-Route::delete('/pembayaran/item-bulk', [PembayaranController::class, 'hapusItemBulk'])
-    ->name('pembayaran.item.bulkDestroy');
-
-
-
-
-
-
-
+    Route::delete('/pembayaran/item/{id}', [PembayaranController::class, 'hapusItem'])
+        ->name('pembayaran.item.destroy');
+    Route::delete('/pembayaran/item-bulk', [PembayaranController::class, 'hapusItemBulk'])
+        ->name('pembayaran.item.bulkDestroy');
 
     Route::resource('sales/data-sales', SalesController::class);
     Route::get('/sales/pengajuan', [PengajuanController::class, 'index'])->name('pengajuan.index');
-    Route::put('/pengeluaran/update-status/{id}', 
+    Route::put('/pengeluaran/update-status/{id}',
         [App\Http\Controllers\PengajuanController::class, 'updateStatus']
     )->name('pengajuan.updateStatus');
-
-
-    
-    
 
     Route::resource('pengaturan/ppn', PpnController::class);
     Route::resource('pengaturan/area', AreaController::class);
     Route::resource('pengaturan/paket-layanan', PaketController::class);
     Route::resource('/tagihan', TagihanController::class);
 
-
     Route::get('/pelanggan/list', [PelangganController::class, 'list'])->name('pelanggan.list');
 });
 
 // ===== SALES ROUTES =====
-Route::middleware(['auth','sales'])->group(function () {
+Route::middleware(['auth', 'sales'])->group(function () {
     // Dashboard sales
-    Route::get('/dashboard/sales', function(){
+    Route::get('/dashboard/sales', function () {
         return view('sales.dashboard');
     })->name('dashboard-sales');
 
